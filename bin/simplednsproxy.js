94b122ad2e17a37d40d7bf36b067cd13104b0050
heruoxin <heruoxin@gmail.com>
2014-06-17 23:19:07 +0800

    Muti-query support

--- bin/simplednsproxy.js
+++ bin/simplednsproxy.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
+var exec = require('child_process').exec;
 var simplednsproxy = require('../lib/dns_server');
+
+exec("whoami ", function(err, stdout, stderr){
+  if (stdout != root){
+    console.warn("Warning: Can't bind 53 port without ROOT permission")
+  }
+});
 
 simplednsproxy(function(err){
   if (err){console.error(err);}
