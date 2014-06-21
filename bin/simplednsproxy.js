aaaf59680874dd36a0adc1cb9d22ad2c79ec67ee
heruoxin <heruoxin@gmail.com>
2014-06-21 13:39:43 +0800

    fix warning of permission.

--- bin/simplednsproxy.js
+++ bin/simplednsproxy.js
@@ -4,12 +4,13 @@ var exec = require('child_process').exec;
 var simplednsproxy = require('../lib/dns_server');
 
 exec("whoami ", function(err, stdout, stderr){
-  if (stdout != root){
+  if (stdout.indexOf("root")){
     console.warn("Warning: Can't bind 53 port without ROOT permission");
   }
 });
 
 
+console.log(new Date().toLocaleString(), "Server Start");
 simplednsproxy(function(err){
   if (err){console.error(err);}
 });
