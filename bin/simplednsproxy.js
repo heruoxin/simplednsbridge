9a2bdde34f4f1bda52e7cd0587324e30fae55576
heruoxin <heruoxin@gmail.com>
2014-06-21 11:02:19 +0800

    open the chche option

--- bin/simplednsproxy.js
+++ bin/simplednsproxy.js
@@ -5,9 +5,10 @@ var simplednsproxy = require('../lib/dns_server');
 
 exec("whoami ", function(err, stdout, stderr){
   if (stdout != root){
-    console.warn("Warning: Can't bind 53 port without ROOT permission")
+    console.warn("Warning: Can't bind 53 port without ROOT permission");
   }
 });
+
 
 simplednsproxy(function(err){
   if (err){console.error(err);}
