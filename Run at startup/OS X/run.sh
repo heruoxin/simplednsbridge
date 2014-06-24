6f5fb246da85fe812fd40d062c3bf4f9ef0233b2
heruoxin <heruoxin@gmail.com>
2014-06-24 21:20:17 +0800

    rename

--- Run at startup/OS X/run.sh
+++ Run at startup/OS X/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo cp ./com.1ittlecup.simplednsproxy.plist /Library/LaunchDaemons/com.1ittlecup.simplednsproxy.plist
-sudo chomd 644 /Library/LaunchDaemons/com.1ittlecup.simplednsproxy.plist
-sudo chown root:wheel /Library/LaunchDaemons/com.1ittlecup.simplednsproxy.plist
-sudo launchctl load /Library/LaunchDaemons/com.1ittlecup.simplednsproxy.plist
+sudo cp ./com.1ittlecup.simplednsbridge.plist /Library/LaunchDaemons/com.1ittlecup.simplednsbridge.plist
+sudo chomd 644 /Library/LaunchDaemons/com.1ittlecup.simplednsbridge.plist
+sudo chown root:wheel /Library/LaunchDaemons/com.1ittlecup.simplednsbridge.plist
+sudo launchctl load /Library/LaunchDaemons/com.1ittlecup.simplednsbridge.plist
 
